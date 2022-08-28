cdc0f61720f1c40013cdf5b2554f01d2ff2a6a2d
angelsflyinhell <azami@namespace.media>
2022-08-28 17:38:35 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let pre_calc_time = SystemTime::now();
     scan_ips();
     println!(
-        "Scanned the internet in {:?} seconds",
+        "Scanned the internet in {:?}",
         SystemTime::now().duration_since(pre_calc_time).unwrap()
     );
 }
@@ -14,20 +14,23 @@ fn main() {
 fn scan_ips() {
     let mut threads: Vec<thread::JoinHandle<()>> = vec![];
 
-    for mon_one in (1..223).rev() {
-        threads.push(thread::spawn(move || {
-            for mon_two in (0..255).rev() {
-                for mon_three in (0..255).rev() {
-                    for mon_four in (0..255).rev() {
+    for mon_one in (1..224).rev() {
+        for mon_two in (0..256).rev() {
+            threads.push(thread::spawn(move || {
+                for mon_three in (0..256).rev() {
+                    for mon_four in (0..256).rev() {
                         // println!(
                         //     "{}.{}.{}.{}\r",
                         //     mon_one, mon_two, mon_three, mon_four
                         // );
-                        request_ip(&format!("{}.{}.{}.{}", mon_one, mon_two, mon_three, mon_four));
+                        request_ip(&format!(
+                            "{}.{}.{}.{}",
+                            mon_one, mon_two, mon_three, mon_four
+                        ));
                     }
                 }
-            }
-        }));
+            }));
+        }
     }
 
     for thread in threads {
@@ -47,13 +50,9 @@ fn request_ip(ip: &str) {
                         println!("{}", ip);
                     }
                 }
-                Err(e) => {
-                    
-                }
+                Err(e) => {}
             }
         }
-        Err(e) => {
-            
-        }
+        Err(e) => {}
     }
 }
